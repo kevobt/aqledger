95279f3fae9dd97c4cdf4056ea6fc3e6c5f0712a
kevobt <kevobt@protonmail.com>
2019-08-23 21:22:13 +0200

    Initialize main command

---
+++ cmd/root.go
@@ -0,0 +1,30 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "hugo",
+	Short: "aqledger is a tool to get transactions using HBCI and convertig them into ledger",
+	Long:  "aqledger is a tool to get transactions using HBCI and convertig them into ledger",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print("Hello World")
+	},
+}
+
+// Execute runs the root commande
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/kevobt/aqledger/cmd"
+
+func main() {
+	cmd.Execute()
+}
